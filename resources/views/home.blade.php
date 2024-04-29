5bc8a5b31a41df5370a0f9d9079d45a871902965
Code-seven123 <manu024595@gmail.com>
2024-04-29 11:26:40 +0700

    adding view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,6 +10,28 @@
         @endif
     </div>
 
-    @dd($siswa)
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <td>NIS</td>
+                <td>NAMA</td>
+                <td>JENIS KELAMIN</td>
+                <td>KELAS</td>
+                <td>ALAMAT</td>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach ($siswa as $item)
+                <tr>
+                    <td>{{ $item->nis }}</td>
+                    <td>{{ $item->nama }}</td>
+                    <td>{{ $item->jk == '1' ? 'laki laki' : 'perempuan' }}</td>
+                    <td>{{ $item->kelas }}</td>
+                    <td>{{ $item->alamat }}</td>
+                </tr>
+            @endforeach
+          </tbody>
+    </table>
+
 </div>
 @endsection
